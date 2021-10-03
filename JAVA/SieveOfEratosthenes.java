9f84ec74522a8739a91e34265e90ed0f962bae16
Saurabh Kumar Kaushal <56887943+saurabh-bot@users.noreply.github.com>
2021-10-03 10:23:07 +0530

    improved indentation in SieveOfEratosthenes.java

--- JAVA/SieveOfEratosthenes.java
+++ JAVA/SieveOfEratosthenes.java
@@ -23,20 +23,17 @@ class SieveOfEratosthenes
         boolean prime[] = new boolean[n + 1];
         
         for (int i = 0; i <= n; i++)
-            prime[i] = true;
+            	prime[i] = true;
  
-        for (int p = 2; p * p <= n; p++) 
-        {
-            if (prime[p] == true)
-            {
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p] == true){
                 // all multiples of p cannot be prime
                 for (int i = p * p; i <= n; i += p)
                     prime[i] = false;
             }
         }
  
-        for (int i = 2; i <= n; i++)
-        {
+        for (int i = 2; i <= n; i++){
             if (prime[i] == true)
                 list.add(i);
         }
